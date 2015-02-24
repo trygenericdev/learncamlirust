70ef51dac1b52ce4274c121b6765ef74edaceace
Generic <trygenericdev@gmail.com>
2015-02-24 00:45:33 -0300

    Changed to print only the number of splits

--- examples/roll.rs
+++ examples/roll.rs
@@ -10,13 +10,15 @@ fn main() {
   let mut file = BufferedReader::new(File::open(&path));
   let mut rs = RollSum::new();
 
+  let mut splits = 0u32;
   loop {
       let b = file.read_byte();
       match b {
         Ok(cb) => {
             rs.roll(cb);
             if rs.on_split() {
-              println!("{} - {}", rs.bits(), rs.digest());
+              //println!("{} - {}", rs.bits(), rs.digest());
+              splits += 1;
             }
           }
         Err(e) => {
@@ -24,4 +26,5 @@ fn main() {
           }
       }
   }
+  println!("Splits: {}", splits)
 }
